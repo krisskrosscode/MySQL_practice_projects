c308a5e27addebc833242c8f0b2e36e8b6a45ad3
krisskrosscode <harshalparteke@gmail.com>
2022-07-21 20:29:23 +0530

    done

--- self_practice/films_practice/lco_films_ans.sql
+++ self_practice/films_practice/lco_films_ans.sql
@@ -31,14 +31,55 @@ using command [source lco_films_ans.sql]*/
 
 -- TODO : Q4) Get the name of films of the actors who belong to India.
 
--- SELECT actor.actor_id
+
+-- SELECT DISTINCT film.title
+-- FROM film
+-- INNER JOIN film_actor ON film_actor.film_id = film.film_id
+-- INNER JOIN actor ON actor.actor_id = film_actor.actor_id
+-- INNER JOIN address ON address.address_id = actor.address_id
+-- INNER JOIN city ON city.city_id = address.city_id
+-- INNER JOIN country ON country.country_id = city.country_id
+-- WHERE country.country = 'India';
+
+
+-- TODO : Q5) How many actors are from the United States?
+
+-- SELECT COUNT(actor.actor_id)
 -- FROM actor
--- INNER JOIN address ON actor.address_id = address.address_id
--- INNER JOIN city ON address.city_id = city.city_id
--- INNER JOIN country ON city.country_id = country.country_id 
--- WHERE country.country = 'India'; 
+-- INNER JOIN address ON address.address_id = actor.address_id
+-- INNER JOIN city ON city.city_id = address.city_id
+-- INNER JOIN country on country.country_id = city.country_id
+-- WHERE country.country = 'United States';
 
 
--- SELECT film.title
--- FROM film_id
--- INNER JOIN film_actor ON film.film_id = film_actor.film_id;
+-- TODO : Q6) Get all languages in which films are released in the year between 2001 and 2010.
+
+-- SELECT language.name AS language, film.release_year, COUNT(language.language_id) AS no_of_films
+-- FROM language
+-- INNER JOIN film ON film.language_id = language.language_id
+-- WHERE film.release_year BETWEEN 2001 AND 2010
+-- GROUP BY language.language_id
+-- ORDER BY film.release_year;
+
+-- TODO : Q7) The film ALONE TRIP (id:17) was actually released in Mandarin, update the info.
+
+-- UPDATE film
+-- SET language_id = (SELECT language_id FROM language WHERE name = 'Mandarin')
+-- WHERE film_id = 17;
+
+-- TODO : Q8) Fetch cast details of films released during 2005 and 2015 with PG rating.
+
+-- SELECT actor.actor_id, CONCAT(actor.first_name, ' ', actor.last_name), film.title
+-- FROM film 
+-- INNER JOIN film_actor ON film_actor.film_id = film.film_id
+-- INNER JOIN actor ON actor.actor_id = film_actor.actor_id
+-- WHERE film.rating = 'PG';
+
+-- TODO : Q9) In which year most films were released?
+
+-- SELECT release_year as year_with_most_films, COUNT(release_year) AS no_of_films 
+-- FROM film 
+-- GROUP BY release_year
+-- ORDER BY COUNT(release_year) DESC 
+-- LIMIT 1; 
+
