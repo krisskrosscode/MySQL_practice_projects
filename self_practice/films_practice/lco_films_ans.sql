8c902a137c5cd992c6c74d6bfd205640de3a7838
krisskrosscode <harshalparteke@gmail.com>
2022-07-23 08:41:52 +0530

    done

--- self_practice/films_practice/lco_films_ans.sql
+++ self_practice/films_practice/lco_films_ans.sql
@@ -101,8 +101,104 @@ using command [source lco_films_ans.sql]*/
 -- LIMIT 1;
 
 
+-- TODO : Q12) Get all Sci- Fi movies with NC-17 ratings and language they are screened in.
+
+-- SELECT film.title, language.name
+-- FROM film
+-- INNER JOIN language ON language.language_id = film.language_id
+-- INNER JOIN film_category ON film_category.film_id = film.film_id
+-- INNER JOIN category ON category.category_id = film_category.category_id 
+-- WHERE category.name = 'Sci-Fi' AND film.rating = 'NC-17';
 
 
 
 
 
+-- TODO : Q13) The actor FRED COSTNER (id:16) shifted to a new address:
+-- TODO :      055,  Piazzale Michelangelo, Postal Code - 50125 , District - Rifredi at Florence, Italy. 
+-- TODO : Insert the new city and update the address of the actor.
+
+-- INSERT INTO city(city, country_id) VALUES("Florence", (SELECT country_id FROM country WHERE country = "Italy"))
+
+-- UPDATE address 
+-- SET address.address = "055,  Piazzale Michelangelo", 
+-- address.postal_code = "50125" , 
+-- address.district =  "Rifredi",
+-- address.city_id = (SELECT city_id FROM city WHERE city = "FLorence")
+-- WHERE address_id = (SELECT address_id FROM actor WHERE actor_id = 16);
+
+
+-- TODO : Q 14) 
+-- TODO : A new film “No Time to Die” is releasing in 2020 whose details are : 
+-- TODO : Title :- No Time to Die
+-- TODO : Description: Recruited to rescue a kidnapped scientist, globe-trotting spy James Bond finds himself hot on the trail of a mysterious villain, who's armed with a dangerous new technology.
+-- TODO : Language: English
+-- TODO : Org. Language : English
+-- TODO : Length : 100
+-- TODO : Rental duration : 6
+-- TODO : Rental rate : 3.99
+-- TODO : Rating : PG-13
+-- TODO : Replacement cost : 20.99
+-- TODO : Special Features = Trailers,Deleted Scenes
+-- TODO : Insert the above data.
+
+-- INSERT INTO film(
+--         title, 
+--         description, 
+--         language_id, 
+--         original_language_id, 
+--         length, 
+--         rental_duration,
+--         rental_rate,
+--         rating,
+--         replacement_cost,
+--         special_features,
+--         release_year
+
+-- ) VALUES(
+--         "No Time to Die",
+--         "Recruited to rescue a kidnapped scientist, globe-trotting spy James Bond finds himself hot on the trail of a mysterious villain, who's armed with a dangerous new technology",
+--         (SELECT language_id FROM language WHERE name = "English"),
+--         (SELECT language_id FROM language WHERE name = "English"),
+--         100,
+--         6,
+--         3.99,
+--         "PG-13",
+--         20.99,
+--         "Trailers,Deleted Scenes",
+--         2020
+-- );
+
+
+-- TODO : Q15) Assign the category Action, Classics, Drama  to the movie “No Time to Die” .
+
+-- INSERT INTO film_category(film_id, category_id)
+-- VALUES
+--         ((SELECT film_id FROM film WHERE title = "No Time to Die"),(SELECT category_id FROM category WHERE name = "Action")),
+--         ((SELECT film_id FROM film WHERE title = "No Time to Die"),(SELECT category_id FROM category WHERE name = "Classics")),
+--         ((SELECT film_id FROM film WHERE title = "No Time to Die"),(SELECT category_id FROM category WHERE name = "Drama"))
+
+-- ;
+
+-- TODO : Q16) Assign the cast: PENELOPE GUINESS, NICK WAHLBERG, JOE SWANK to the movie “No Time to Die” .
+
+-- INSERT INTO film_actor(actor_id, film_id)
+-- VALUES ((SELECT actor_id FROM actor WHERE first_name = "PENELOPE" AND last_name = "GUINESS"), (SELECT film_id FROM film WHERE title = "No Time to Die")),
+-- ((SELECT actor_id FROM actor WHERE first_name = "NICK" AND last_name = "WAHLBERG"), (SELECT film_id FROM film WHERE title = "No Time to Die")),
+-- ((SELECT actor_id FROM actor WHERE first_name = "JOE" AND last_name = "SWANK"), (SELECT film_id FROM film WHERE title = "No Time to Die"));
+
+
+-- TODO : Q17) Assign a new category Thriller  to the movie ANGELS LIFE.
+
+-- INSERT INTO category(name) VALUES ("Thriller");
+
+-- INSERT INTO film_category(film_id, category_id)
+-- VALUES ((SELECT film_id FROM film WHERE title = "ANGELS LIFE" ), (SELECT category_id FROM category WHERE name = "THriller"));
+
+        -- To check if the new data is added, run the following query: 
+        
+-- SELECT film.title, category.name as Category
+-- FROM film
+-- INNER JOIN film_category ON film.film_id = film_category.film_id
+-- INNER JOIN category ON category.category_id = film_category.category_id
+-- WHERE film.title = "ANGELS LIFE";
